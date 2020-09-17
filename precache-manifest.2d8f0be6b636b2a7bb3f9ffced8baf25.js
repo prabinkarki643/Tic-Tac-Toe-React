b1725d51275ebefaef2e6cb23b4db132d0321032
githubprabin143 <prabinkarki643@gmail.com>
2020-09-17 10:24:14 +0530

    Updates

---
+++ precache-manifest.2d8f0be6b636b2a7bb3f9ffced8baf25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b43f16c95bdf4b47f71e60450a818df",
+    "url": "/Tic-Tac-Toe-React/index.html"
+  },
+  {
+    "revision": "a1c26c874e08e68d8d23",
+    "url": "/Tic-Tac-Toe-React/static/css/main.ed90724c.chunk.css"
+  },
+  {
+    "revision": "5a90e356bec01e278b69",
+    "url": "/Tic-Tac-Toe-React/static/js/2.ad9f6083.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe-React/static/js/2.ad9f6083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1c26c874e08e68d8d23",
+    "url": "/Tic-Tac-Toe-React/static/js/main.e537c4a4.chunk.js"
+  },
+  {
+    "revision": "f1c7b5fbbbccef32c52d",
+    "url": "/Tic-Tac-Toe-React/static/js/runtime-main.6c16d84a.js"
+  }
+]);
