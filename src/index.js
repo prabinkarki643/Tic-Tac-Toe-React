0d2cd4ba0a1d2765b3a1c406fde84b9fc60c7346
githubprabin143 <prabinkarki643@gmail.com>
2020-09-17 15:10:12 +0530

    Winer Box Highlighted

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function Square({ value, onClick }) {
-
+function Square({ number,value, onClick }) {
   return (
-    <button className="square" onClick={onClick}>
+    <button className="square" id={`square-${number}`} onClick={onClick}>
       {value}
     </button>
   );
@@ -25,10 +24,33 @@ function Game() {
   const [ isXNext, setIsXNext ] = useState(true);
   const nextSymbol = isXNext ? "X" : "O";
   const winner = calculateWinner(squares);
-
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if(winner){
+        var colorsText = document.querySelectorAll("#colorsText");
+        for (let index = 0; index < colorsText.length; index++) {
+          const element = colorsText[index];
+          element.style.color =getRandomColor();
+          element.style.fontSize='40px'
+        }
+      }
+  
+    }, 500);
+    // Specify how to clean up after this effect:
+    return function cleanup() {
+      clearInterval(timer);
+    };
+  });
   function getStatus() {
     if (winner) {
-      return "Winner: " + winner;
+      const [a, b, c] = winner.line;
+      const aEle =  document.querySelector(`#square-${a}`)
+      const bEle =  document.querySelector(`#square-${b}`)
+      const cEle =  document.querySelector(`#square-${c}`)
+      aEle.style.backgroundColor ='#00802b';
+      bEle.style.backgroundColor ='#00802b';
+      cEle.style.backgroundColor ='#00802b';
+      return "Winner: " + winner.name;
     } else if (isBoardFull(squares)) {
       return "Draw!";
     } else {
@@ -39,6 +61,7 @@ function Game() {
   function renderSquare(i) {
     return (
       <Square
+        number={i}
         value={squares[i]}
         onClick={() => {
           if (squares[i] != null || winner != null) {
@@ -54,10 +77,31 @@ function Game() {
     );
   }
 
+  function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
   function renderRestartButton() {
     return (
       <Restart
         onClick={() => {
+          var colorsText = document.querySelectorAll("#colorsText");
+          for (let index = 0; index < colorsText.length; index++) {
+            const element = colorsText[index];
+            element.style.color ='black'
+          }
+          const [a, b, c] = winner.line;
+          const aEle =  document.querySelector(`#square-${a}`)
+          const bEle =  document.querySelector(`#square-${b}`)
+          const cEle =  document.querySelector(`#square-${c}`)
+          aEle.style.backgroundColor ='#7299a8';
+          bEle.style.backgroundColor ='#7299a8';
+          cEle.style.backgroundColor ='#7299a8';
           setSquares(Array(9).fill(null));
           setIsXNext(true);
         }}
@@ -85,7 +129,7 @@ function Game() {
             {renderSquare(8)}
           </div>
         </div>
-        <div className="game-info">{getStatus()}</div>
+        <div className="game-info" id="colorsText">{getStatus()}</div>
         <div className="restart-button">{renderRestartButton()}</div>
       </div>
     </div>
@@ -109,7 +153,8 @@ function calculateWinner(squares) {
   for (let i = 0; i < possibleLines.length; i++) {
     const [a, b, c] = possibleLines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+ 
+      return {name:squares[a],line:possibleLines[i]}
     }
   }
   return null;
